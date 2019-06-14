a09e2aa0cfca59f2a9763f214a14ff8a692e0dea
Vinod4000 <rkvinod15@gmail.com>
2019-06-14 10:57:28 -0400

    be4lunch
    done with rps

--- RockPaperScissors/src/RPS_Code.java
+++ RockPaperScissors/src/RPS_Code.java
@@ -1,33 +1,84 @@
 
 public class RPS_Code {
-	public int wins;
-	public int losses;
-	public int ties;
-	public String choice;
+//	private int wins;
+//	private int losses;
+//	private int ties;
+	private int[] history = new int[3];
+	private String choice;
 	
 	RPS_Code() {
-		wins = 0;
-		losses = 0;
-		ties = 0;
+//		wins = 0;
+//		losses = 0;
+//		ties = 0;
+		history = new int[] {0, 0, 0};
 		choice = "";
 
 	}
 	
 	RPS_Code (String choice) {
 		this.choice = choice;
+//		this.wins = 0;
+//		this.losses = 0;
+//		this.ties = 0;
+		this.history[0] = 0;
 	}
 	
-	void correctChoice(String cho, String comp) {
-		if (cho == "rock" && comp == "paper" || cho == "paper" && comp == "rock" || cho == "scissors" && comp == "paper") {
-			wins += 1;
+	public int correctChoice(String cho, String comp) {
+		if ((cho.equals("rock") && comp.equals("scissors")) || (cho.equals("paper") && comp.equals("rock")) || (cho.equals("scissors") && comp.equals("paper"))) {
+//			this.wins += 1;
+			this.history[0]++;
+			return 1;
 		}
-		if (cho.equals(comp)) {
-			ties += 1;
+		else if (cho.equals(comp)) {
+//			this.ties += 1;
+			this.history[2]++;
+			return 2;
 		}
 		else {
-			losses += 1;
+//			this.losses += 1;
+			this.history[1]++;
+			return 0;
 		}
 	}
+	
+	public String generateComp() {
+		String comp = "";
+		int rand = (int) (3*Math.random());
+		if (rand == 0) {
+			comp = "rock";
+		}
+		if (rand == 1) {
+			comp = "paper";
+		}
+		if (rand == 2) {
+			comp = "scissors";
+		}
+		return comp;
+	}
+	
+	public int getWins() {
+		return this.history[0];
+	}
+	
+	public int getLosses() {
+		return this.history[1];
+	}
+	
+	public int getTies() {
+		return this.history[2];
+	}
+	
+//	public void setWins(int wins) {
+//		this.wins = wins;
+//	}
+//	
+//	public void setLosses(int losses) {
+//		this.losses = losses;
+//	}
+//	
+//	public void setTies(int ties) {
+//		this.ties = ties;
+//	}
 	public static void main (String[] args) {
 		
 	}
--- RockPaperScissors/src/RPS_Game.java
+++ RockPaperScissors/src/RPS_Game.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+import java.util.ArrayList; 
 
 public class RPS_Game {
+	static ArrayList<String> arr = new ArrayList<>();
+	
+	static void storeArr(int winner) {
+		if (winner == 1) {
+			arr.add("Player win");
+		}
+		else if (winner == 0) {
+			arr.add("Computer win");
+		}
+		else if (winner == 2) {
+			arr.add("It was a tie");
+		}
+	}
 
 	public static void main(String[] args) {
+		String compChoice = "";
+		RPS_Code rps = new RPS_Code();
+		String choice = "";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to the RPS Simulation");
 		for (int i = 0; i < 10; i++) {
-			RPS_Game game = new RPS_Game("rock");
+			System.out.println("Round " + (i + 1) + "!");
+			System.out.println("What's your choice: rock, paper, or scissors?");
+			choice = scan.nextLine();
+			compChoice = rps.generateComp();
+			System.out.println("You picked " + choice + ". The computer picked " + compChoice + ".");
+			storeArr(rps.correctChoice(choice, compChoice));
+			System.out.println("You have " + rps.getWins() + " win(s), and the computer has " + rps.getLosses() + " win(s). There has been " + rps.getTies() + " tie(s) so far.");
 		}
+		System.out.println("Here is the history of the games.");
+		System.out.println(arr);
 
 	}
 
